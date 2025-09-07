fd66afc8ccf97ad6817d24a5721bb36585403e2d
sangtran13 <tranngocsang147877@gmail.com>
2025-09-07 18:05:31 +0700

    refactor: implement MVC pattern for project structure

---
+++ src/routes/about.js
@@ -0,0 +1,9 @@
+import express from 'express';
+const router = express.Router();
+import AboutController from '../app/controllers/AboutController.js';
+
+router.use('/:slug', AboutController.show);
+router.use('/', AboutController.index);
+
+
+export default router;

f628badeb88c61ec5c811a3873151a9d54b73995
sangtran13 <tranngocsang147877@gmail.com>
2025-09-07 11:41:15 +0700

    Inital commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// install npm express, npm nodemon
+import express from 'express';
+const app = express();
+const port = 3000;
+
+// root route
+app.get('/', (req, res) => res.send('Hello World!'));
+
+// 127.0.0.1 -> localhost
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
